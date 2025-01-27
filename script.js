6734553f15467a11eacc5bdd4100aa336740ba57
Elevathion <coksys.micancion@gmail.com>
2025-01-27 18:49:59 +0100

    version 1.3

--- script.js
+++ script.js
@@ -1,124 +1,123 @@
+window.onload = function() {
+  // Si encontramos el elemento, recuperamos el valor del localStorage
+  const checkedOptions = JSON.parse(localStorage.getItem('checkedCheckboxes'));
+  if (checkedOptions) {
+    loadCheckboxes();}
+  else {
+    webConfig();
+  }
+};
+
+  // devolvemos las casillas de verificación a la última configuración antes de cerrar ventana
+function loadCheckboxes() {
+   /* Recogemos la información del estado de los ckeckbox almacenada en localStorage */
+   const checkedOptions = JSON.parse(localStorage.getItem('checkedCheckboxes'));
+   /* Array de los datos personales*/
+   const datosPublicos = document.querySelectorAll('.toggleDiv');
+   // activamos o no checkbox según info del localStorage
+   checkedOptions.forEach(function(checkbox, indice) {
+    if (checkbox=="on") {
+      datosPublicos[indice].checked = true;}
+    else {
+      datosPublicos[indice].checked = false;
+    };
+  actualizar();
+  });
+};
+
 /* Botón configuración del perfil público */
+  const configPublic = document.getElementById("configPublic");
+  const cambiaConfig = configPublic.addEventListener('click', webConfig);
 
-const configPublic = document.getElementById("configPublic");
-const cambiaConfig = configPublic.addEventListener('click', webConfig);
-
+// activa menú "configurar datos visibles"
 function webConfig() {
   const checkDiv = document.getElementById("publiSelect");
   const datosDiv = document.getElementById("datosPublicos");
   checkDiv.style.display = 'block';
   datosDiv.style.display = 'none';
-}
+};
 
-  /* Visualización individual de los elementos del perfil */
+/* Visualización individual de los elementos del perfil */
   const checkboxes = document.querySelectorAll('.toggleDiv');
-  
   checkboxes.forEach(function(checkbox) {
-      
-          const targetId = checkbox.getAttribute('data-target');
-          const targetElement = document.getElementById(targetId);
-  /* en el condicional se visualizan los datos de cada div según esté chequeada la casilla o no */          
-          if (checkbox.checked) {
-            targetElement.style.display = 'block';}
-          else {
-            targetElement.style.display = 'none';
-          };
-        });
-
-        /* Visualización total o ninguna de los elementos del perfil BUTTON*/
-
-        /* función del botón id=allDiv */
-        function botonTodo () {
-        const checkboxes = document.querySelectorAll('.toggleDiv');
-        checkboxes.forEach(function(checkbox) {
-  
-          const targetId = checkbox.getAttribute('data-target');
-          const targetElement = document.getElementById(targetId);
-
-          /* Visualización total */
-            checkbox.checked = true;
-            targetElement.style.display = 'block';
-          }
-        )};
-        
-        /* función del botón id=noDiv */
-        function botonNada () {
-
-          const checkboxes = document.querySelectorAll('.toggleDiv');
-          checkboxes.forEach(function(checkbox) {
-    
-            const targetId = checkbox.getAttribute('data-target');
-            const targetElement = document.getElementById(targetId);
-  
-            /* Visualización ninguna */
-              checkbox.checked = false;
-              targetElement.style.display = 'none';
-            }
-          )};
-
-    /* botón guardar cambios */
-        function actualizar() {
-          
-          const checkboxes = document.querySelectorAll('.toggleDiv');
-  
-          checkboxes.forEach(function(checkbox) {
-      
-          const targetId = checkbox.getAttribute('data-target');
-          const targetElement = document.getElementById(targetId);
-          //en el condicional se visualizan los datos de cada div
-          // según esté chequeada la casilla o no          
-          if (checkbox.checked) {
-            targetElement.style.display = 'block';}
-          else {
-            targetElement.style.display = 'none';
-          };
-        });
-          saveCheckboxes();
-          const checkDiv = document.getElementById("publiSelect");
-          const datosDiv = document.getElementById("datosPublicos");
-          datosDiv.style.display = 'block';
-          checkDiv.style.display = 'none';
-        
-        
-        // Array de los checkbox
-        function saveCheckboxes() {
-           // const checkboxes = document.querySelectorAll('.toggleDiv');
-            
-            let checkedOptions = [];
-            
-            checkboxes.forEach(function(checkbox) {
-              const targetId = checkbox.getAttribute('data-target');
-              const targetElement = document.getElementById(targetId);
-              
-          //añadimos cada valor del array checkedoptions
-              if (checkbox.checked) {
-              checkbox.value="on";
-              }
-              else {
-              checkbox.value="off";
-              };
-              checkedOptions.push(checkbox.value);
-              console.log(checkbox.value);
-              console.log(checkedOptions);
-          });
-          
-          localStorage.setItem('checkedCheckboxes', JSON.stringify(checkedOptions));
-          /*if (targetWindow) {
-                  targetWindow.postMessage(checkedOptions);
-              } else {
-                  alert('No se pudo abrir otra ventana.');
-              }*/
-          
-      };
-  
+    const targetId = checkbox.getAttribute('data-target');
+    const targetElement = document.getElementById(targetId);
+  /* se visualizan los datos de cada div según esté chequeada la casilla o no */          
+    if (checkbox.checked){
+      targetElement.style.display = 'block';}
+    else {
+      targetElement.style.display = 'none';
     };
+  });
+/* Visualización total o ninguna de los elementos del perfil (botones seleccionar todo/nada) */
+
+  /* función del botón id=allDiv seleccionar todo */
+function botonTodo () {
+    const checkboxes = document.querySelectorAll('.toggleDiv');
+    checkboxes.forEach(function(checkbox) {
+      const targetId = checkbox.getAttribute('data-target');
+      const targetElement = document.getElementById(targetId);
+      /* Visualización total */
+      checkbox.checked = true;
+      targetElement.style.display = 'block';
+    }
+    )};
+        
+  /* función del botón id=noDiv deseleccionar todo */
+function botonNada () {
+    const checkboxes = document.querySelectorAll('.toggleDiv');
+    checkboxes.forEach(function(checkbox) {
+      const targetId = checkbox.getAttribute('data-target');
+      const targetElement = document.getElementById(targetId);
+      /* Visualización ninguna */
+        checkbox.checked = false;
+        targetElement.style.display = 'none';
+    }
+)};
+
+  /* botón guardar cambios */
+function actualizar() {
+    const checkboxes = document.querySelectorAll('.toggleDiv');
+    checkboxes.forEach(function(checkbox) {
+      const targetId = checkbox.getAttribute('data-target');
+      const targetElement = document.getElementById(targetId);
+      /*en el condicional se visualizan los datos de cada div
+      según esté chequeada la casilla o no */         
+      if (checkbox.checked) {
+        targetElement.style.display = 'block';}
+      else {
+        targetElement.style.display = 'none';
+      };
+    });
+    saveCheckboxes(); // guardamos el estado de cada checkbox
+    const checkDiv = document.getElementById("publiSelect");
+    const datosDiv = document.getElementById("datosPublicos");
+    datosDiv.style.display = 'block';
+    checkDiv.style.display = 'none';
+};
+
+// guardamos el estado de cada checkbox
+function saveCheckboxes() {
+  const checkboxes = document.querySelectorAll('.toggleDiv');
+  let checkedOptions = [];
+  checkboxes.forEach(function(checkbox) {
+    const targetId = checkbox.getAttribute('data-target');
+    const targetElement = document.getElementById(targetId);
+    //añadimos cada valor del array checkedoptions
+    if (checkbox.checked) {
+    checkbox.value="on";
+    }
+    else {
+    checkbox.value="off";
+    };
+     checkedOptions.push(checkbox.value);
+  });
+ localStorage.setItem('checkedCheckboxes', JSON.stringify(checkedOptions));
+};
 
 
         
-        /* const actualizaTodo = document.getElementById("allDiv", actualizar);
-        const actualizaNada = document.getElementById("noDiv", actualizar);
-        actualizaTodo.addEventListener('click', actualizar);
-        actualizaNada.addEventListener('click', actualizar); */
+        
 
 
 
